74eb2b489818f747b5ac89fde7c8f6c3848788b0
mevans7777 <michael.evans@vml.com>
2025-07-07 10:05:14 +0100

    feat: example 1

--- web/src/index.js
+++ web/src/index.js
@@ -14,7 +14,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// First feature test
-// Second feature test
-// Fix example fix one
+// web feature 1
 reportWebVitals();
